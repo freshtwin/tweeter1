6f91ce11799d6e399bae4ec651eaa15cc51bdfb3
Kylie Mills <kmil134@lsu.edu>
2020-02-18 15:59:10 -0600

    why

--- tweets/views.py
+++ tweets/views.py
@@ -2,24 +2,24 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse
-from django.contrib.auth.mixins import LoginRequiredMixin #new
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Tweet
 
 class TweetListView(ListView):
     model = Tweet
     template_name = 'home.html'
 
-class TweetCreateView(LoginRequiredMixin, CreateView): # new
+class TweetCreateView(LoginRequiredMixin, CreateView):
     model = Tweet
     template_name = 'tweet_new.html'
-    fields = ['body'] # removed user
+    fields = ['body']
 
     def get_success_url(self):
         return reverse('home')
 
-    def get_login_url(self): # new
-        return reverse('login') # new
+    def get_login_url(self):
+        return reverse('login')
 
-    def form_valid(self, form): # new
-        form.instance.user = self.request.user # new
-        return super().form_valid(form) # new
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
--- users/views.py
+++ users/views.py
@@ -4,34 +4,35 @@ from django.views import generic
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView
-from django.contrib.messages.views import SuccessMessageMixin # new
+from django.contrib.messages.views import SuccessMessageMixin
 
-class SignUpView(SuccessMessageMixin, generic.CreateView): # new
+class SignUpView(SuccessMessageMixin, generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-    success_message = 'Account was created successfully.' # new
+    success_message = 'Account was created successfully.'
 
-class UserUpdateView(SuccessMessageMixin, generic.UpdateView): # new
+class UserUpdateView(SuccessMessageMixin, generic.UpdateView):
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('home')
     template_name = 'update.html'
-    success_message = 'User profile updated.' # new
+    success_message = 'User profile updated.'
 
     # This keeps users from accessing the profile of other users.
     def get_queryset(self):
+        # return CustomUser.objects.all()
         user = self.request.user
         if user.is_superuser:
             return CustomUser.objects.all()
         else:
             return CustomUser.objects.filter(id=user.id)
 
-class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView): # new
+class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView):
     success_url = reverse_lazy('home')
     template_name = 'change_password.html'
-    success_message = 'Password changed.' # new
+    success_message = 'Password changed.'
 
-class UserPasswordResetView(SuccessMessageMixin, PasswordResetView): # new
+class UserPasswordResetView(SuccessMessageMixin, PasswordResetView):
     success_url = reverse_lazy('login')
     template_name = 'reset_password.html'
-    success_message = 'Check your email for a reset link.' # new
+    success_message = 'Check your email for a reset link.'
